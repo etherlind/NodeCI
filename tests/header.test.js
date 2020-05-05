049e58b137bbe20f43252c669bf7ce72433fb53c
camila <camila.yusupova@skitudeservices.com>
2020-05-05 12:58:39 +0200

    configuring Travis CI

---
+++ tests/header.test.js
@@ -0,0 +1,31 @@
+const Page = require('./helpers/page.js');
+
+let page;
+
+beforeEach(async () => {
+    page = await Page.build();
+    await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+    await page.close();
+});
+
+test('the header has the correct text', async () => {
+    const text = await page.getContentsOf('a.brand-logo');
+    expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts the OAuth flow', async () => {
+    await page.click('.right a');
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+
+    await page.login();
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+    expect(text).toEqual('Logout');
+});
---
+++ tests/setup.js
@@ -0,0 +1,12 @@
+// how long it should wait until failing the test
+jest.setTimeout(30000);
+// just requiring it so mongoose knows where User is
+require('../models/User');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+// mongoose doesn't use Promises out of the box
+// so we're telling it to use Node Promise implementation
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
