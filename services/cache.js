7f46fdb885898d475a021aeda1b2d61e285bacc1
camila <camila.yusupova@skitudeservices.com>
2020-05-06 10:34:25 +0200

    fix redis client url

--- services/cache.js
+++ services/cache.js
@@ -3,7 +3,7 @@ const redis = require('redis');
 const util = require('util');
 const keys = require('../config/keys');
 
-const client = redis.createClient(redisUrl);
+const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
