90d21e27f6bbd065e747a0bd89aeea16773b5532
camila <camila.yusupova@skitudeservices.com>
2020-05-06 10:10:06 +0200

    configuring Travis CI

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/blogRoutes')(app);
 
-if (['production'].includes(process.env.NODE_ENV)) {
+if (['production', 'ci'].includes(process.env.NODE_ENV)) {
     app.use(express.static('client/build'));
 
     const path = require('path');
@@ -36,7 +36,7 @@ if (['production'].includes(process.env.NODE_ENV)) {
         res.sendFile(path.resolve('client', 'build', 'index.html'));
     });
 }
-
+// PORT is defined in travis.yml
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening on port`, PORT);
